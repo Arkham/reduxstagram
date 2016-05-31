b15995318414f2655d4ea7d5cc14208612d6a7f1
Ju Liu <ju.liu@welaika.com>
2016-05-31 12:40:52 +0100

    Work on single view layout

---
+++ client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    return (
+      <div className="comments">
+        I'm the comments
+      </div>
+    );
+  }
+});
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 export default React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => {
+      return post.code === this.props.params.postId
+    });
+
+    const post = this.props.posts[i];
+
     return (
       <div className="single-photo">
-        I'm a single photo
+        <Photo {...this.props} i={i} post={post} />
+        <Comments {...this.props} i={i} post={post} />
       </div>
     );
   }
