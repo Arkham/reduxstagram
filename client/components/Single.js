cdfc72fb1e46a4cd96d680948ac7699e3aaf999f
Ju Liu <ju.liu@welaika.com>
2016-06-10 08:20:25 +0200

    Create comments component

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,16 +4,18 @@ import Comments from './Comments';
 
 export default React.createClass({
   render() {
+    const postId = this.props.params.postId;
     const i = this.props.posts.findIndex((post) => {
-      return post.code === this.props.params.postId
+      return post.code === postId
     });
 
     const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
 
     return (
       <div className="single-photo">
         <Photo {...this.props} i={i} post={post} />
-        <Comments {...this.props} i={i} post={post} />
+        <Comments postComments={postComments}/>
       </div>
     );
   }
